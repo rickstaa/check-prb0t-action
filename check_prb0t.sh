452f286aec05e168853053c245cf0c646373cac9
rickstaa <rick.staa@outlook.com>
2023-02-17 12:02:59 +0100

    feat: add action core

---
+++ check_prb0t.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get main author.
+authors=$(git log -1 --pretty=format:'%an ')
+
+# Get co-authors.
+authors+=$(git log -1 --pretty=format:'%b')
+
+# Check if PRB0T is in the list of authors.
+if [[ $authors =~ "PRB0T" ]]; then
+    used_prbot="true"
+    echo "PRB0T is in the list of authors."
+else
+    used_prbot="false"
+    echo "PRB0T is not in the list of authors."
+fi
+
+# Set output and environment variable.
+echo "used_prb0t=${used_prbot}" >> $GITHUB_OUTPUT
+echo "USED_PRB0T=${used_prbot}" >> $GITHUB_ENV
+exit 0
