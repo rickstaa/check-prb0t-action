d5dce1bb5b18703710447194e84377ac53abf80f
rickstaa <rick.staa@outlook.com>
2023-02-20 13:52:16 +0100

    feat: simplify github action
    Since PRb0t currently does not allow setting the author we can simplify
    the action for now.

--- check_prb0t.sh
+++ check_prb0t.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# Get main author.
-authors=$(git log -1 --pretty=format:'%an ')
-
-# Get co-authors.
-authors+=$(git log -1 --pretty=format:'%b')
-
 # Check if PRB0T is in the list of authors.
-if [[ $authors =~ "PRB0T" ]]; then
+if [[ $GITHUB_ACTOR =~ "PRb0t" ]]; then
     used_prbot="true"
-    echo "PRB0T is in the list of authors."
+    echo "Pull request was created using PRb0t."
 else
     used_prbot="false"
-    echo "PRB0T is not in the list of authors."
+    echo "Pull request was not created using PRb0t."
 fi
 
 # Set output and environment variable.
